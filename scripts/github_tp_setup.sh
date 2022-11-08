2759ebc35f3371f0a6d277434d1a8607458a9cfa
Alessandro Franceschi <al@example42.com>
2022-11-08 11:18:32 +0100

    Change order of things

--- scripts/github_tp_setup.sh
+++ scripts/github_tp_setup.sh
@@ -2,13 +2,14 @@
 PATH=/opt/puppetlabs/puppet/bin:/usr/local/sbin:$PATH
 wget -O - https://bit.ly/installpuppet | sudo bash
 
+# Let's use tinydaya from the PR
+sudo mkdir -p /etc/puppetlabs/code/modules/tinydata/
+sudo ln -s /home/runner/work/tinydata/tinydata/data /etc/puppetlabs/code/modules/tinydata/data
+
 echo "### Installing Tiny Puppet"
 sudo puppet module install example42-tp --ignore-dependencies
 sudo puppet module install puppetlabs-stdlib --ignore-dependencies
 sudo puppet tp setup
 
-# Let's use tinydaya from the PR
-sudo mkdir -p /etc/puppetlabs/code/modules/tinydata/
-sudo ln -s /home/runner/work/tinydata/tinydata/data /etc/puppetlabs/code/modules/tinydata/data
 
 exit 0

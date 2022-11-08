02e9824e8c1af0a287397d08927e936e11fef5d2
Alessandro Franceschi <al@example42.com>
2022-11-08 09:26:01 +0100

    Fixing changes detection. Alt approach

--- scripts/tp_test.sh
+++ scripts/tp_test.sh
@@ -6,10 +6,12 @@ repo_dir="$(dirname "${0}")/.."
 . "${repo_dir}/scripts/functions"
 
 # Detecting apps for which we have changes in the PR
-diff_commits_number=$(git log $default_branch.."${1}" --pretty=oneline | wc -l)
-echo "Checking for files in the last $diff_commits_number commits"
-changedfiles=$(git diff HEAD~"${diff_commits_number}" --name-only | grep 'data/');
-apps=$(echo "$changedfiles" | cut -d '/' -f 2 | sort | uniq)
+#diff_commits_number=$(git log $default_branch.."${1}" --pretty=oneline | wc -l)
+#echo "Checking for files in the last $diff_commits_number commits"
+#changedfiles=$(git diff HEAD~"${diff_commits_number}" --name-only | grep 'data/');
+changedfiles=$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD $default_branch))
+
+apps=$(echo "$changedfiles" | grep '^data' | cut -d '/' -f 2 | sort | uniq)
 
 for app in $apps; do
   echo_title "Testing $app"

a04eb338206fc97f72be5b29eef0f358793b08b1
Alessandro Franceschi <al@example42.com>
2022-11-08 09:07:30 +0100

    Fix github action vars

--- scripts/tp_test.sh
+++ scripts/tp_test.sh
@@ -1,26 +1,26 @@
 #!/usr/bin/env bash
 default_branch='master'
-repo_dir="$(dirname $0)/.."
+repo_dir="$(dirname "${0}")/.."
 
 # repo_dir=$(git rev-parse --show-toplevel)
 . "${repo_dir}/scripts/functions"
 
-diff_commits_number=$(git log origin/$default_branch..$1 --pretty=oneline | wc -l)
+diff_commits_number=$(git log origin/$default_branch.."${1}" --pretty=oneline | wc -l)
 echo "Checking for files in the last $diff_commits_number commits"
-changedfiles=$(git diff HEAD~$diff_commits_number --name-only | grep 'data/');
+changedfiles=$(git diff HEAD~"${diff_commits_number}" --name-only | grep 'data/');
 
 apps=$(echo "$changedfiles" | cut -d '/' -f 2 | sort | uniq)
 
 for app in $apps; do
   echo_title "Testing $app"
-  tp install $app
-  tp test $app
-  if [ "x$?" == "x0" ]; then
+  tp install "${app}"
+  
+  if tp test "${app}"; then
     result='success'
   else
     result='failure'
   fi
-  echo_$result "## ${result}! ## Output written to tests/app/$2/${result}/$1"
+  echo_$result "## ${result}! ## Output written to tests/app/${2}/${result}/${1}"
 
 done
 

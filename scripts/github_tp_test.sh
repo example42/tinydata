0cf9e3f0f9169d722696c7ce64f1c71265aee97f
Alessandro Franceschi <al@example42.com>
2022-11-11 20:04:43 +0100

    Lints

--- scripts/github_tp_test.sh
+++ scripts/github_tp_test.sh
@@ -1,18 +1,16 @@
 #!/usr/bin/env bash
 changedfiles="${1}"
-default_branch='master'
 repo_dir="$(dirname "${0}")/.."
-
-# repo_dir=$(git rev-parse --show-toplevel)
 . "${repo_dir}/scripts/functions"
 
 # Detecting apps for which we have changes in the PR
+#default_branch='master'
 #diff_commits_number=$(git log $default_branch.."${1}" --pretty=oneline | wc -l)
 #echo "Checking for files in the last $diff_commits_number commits"
 #changedfiles=$(git diff HEAD~"${diff_commits_number}" --name-only | grep 'data/');
 #changedfiles=$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD origin/$default_branch))
 
-apps=$(for a in $changedfiles ; do echo $a ; done | grep '^data' | cut -d '/' -f 2 | sort | uniq)
+apps=$(for a in $changedfiles ; do echo "$a" ; done | grep '^data' | cut -d '/' -f 2 | sort | uniq)
 
 for app in $apps; do
 

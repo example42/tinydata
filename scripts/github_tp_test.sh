a6f38da8b21fa0d564c0bb691a2bd2676878cd0f
Alessandro Franceschi <al@example42.com>
2022-11-12 18:47:17 +0100

    Added test ci workflow (#52)

---
+++ scripts/github_tp_test.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+changedfiles="${1}"
+repo_dir="$(dirname "${0}")/.."
+. "${repo_dir}/scripts/functions"
+
+# Detecting apps for which we have changes in the PR
+#default_branch='master'
+#diff_commits_number=$(git log $default_branch.."${1}" --pretty=oneline | wc -l)
+#echo "Checking for files in the last $diff_commits_number commits"
+#changedfiles=$(git diff HEAD~"${diff_commits_number}" --name-only | grep 'data/');
+#changedfiles=$(git --no-pager diff --name-only FETCH_HEAD $(git merge-base FETCH_HEAD origin/$default_branch))
+
+apps=$(for a in $changedfiles ; do echo "$a" ; done | grep '^data' | cut -d '/' -f 2 | sort | uniq)
+
+for app in $apps; do
+
+  echo_title "### Checking ${app}"
+  sudo tp install "${app}" || true
+  if sudo tp test "${app}"; then
+    result='success'
+  else
+    result='failure'
+  fi
+  echo_$result "### ${app} test: ${result}!"
+  echo
+  echo_title "### ${app} info"
+  sudo tp info "${app}"
+  echo
+  echo_title "### ${app} version"
+  sudo tp version "${app}"
+  echo
+  echo_title "### ${app} uninstall"
+  sudo tp uninstall "${app}" || true
+  echo
+
+done
